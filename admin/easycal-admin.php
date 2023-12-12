5a333a711abf64d83e29359b60f3a123ee8cf36e
Santiago <santiagobinario@gmail.com>
2023-12-11 22:58:25 -0600

    Modulación

--- admin/easycal-admin.php
+++ admin/easycal-admin.php
@@ -2,80 +2,92 @@
 
 class Easycal_Admin {
 
-	private $plugin_name;
-	private $version;
+    private $plugin_name;
+    private $version;
 
-	public function __construct( $plugin_name, $version ) {
+    public function __construct($plugin_name, $version) {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+    }
 
-		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+    public function easycal_init_post_type() {
+        $labels = array(
+            'name'               => __('Shortcodes'),
+            'singular_name'      => __('Shortcode'),
+            'menu_name'          => __('Shortcodes'),
+            'all_items'          => __('Todos los Shortcodes'),
+            'add_new'            => __('Agregar Nuevo'),
+            'add_new_item'       => __('Agregar Nuevo Shortcode'),
+            'edit_item'          => __('Editar Shortcode'),
+            'new_item'           => __('Nuevo Shortcode'),
+            'view_item'          => __('Ver Shortcode'),
+            'search_items'       => __('Buscar Shortcodes'),
+            'not_found'          => __('No se encontraron shortcodes'),
+            'not_found_in_trash' => __('No se encontraron shortcodes en la papelera'),
+        );
+
+        $args = array(
+            'labels'             => $labels,
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'show_in_menu'       => 'easycal-admin-menu',
+            'query_var'          => true,
+            'rewrite'            => array('slug' => 'shortcode'),
+            'capability_type'    => 'post',
+            'has_archive'        => true,
+            'hierarchical'       => false,
+            'menu_position'      => null,
+            'supports'           => array('title', 'editor'),
+        );
+
+        register_post_type('shortcode', $args);
+    }
+
+    public function easycal_add_admin_menu() {
+        add_menu_page(
+            __('EasyCal', 'easy-cal'),
+            __('EasyCal', 'easy-cal'),
+            'manage_options',
+            'easycal-admin-menu',
+            array($this, 'easycal_admin_menu_page'),
+            'dashicons-calendar-alt',
+            30
+        );
+    }
+
+    public function easycal_admin_menu_page() {
+        $render = '<div class="page-main"></div>';
+        echo wp_kses($render, 'h1');
+    }
+
+    public function easycal_generate_and_save_shortcode($post_id) {
+        if (get_post_type($post_id) !== 'shortcode') {
+            return;
+        }
+
+        $shortcode = '[mi_shortcode id="' . $post_id . '"]';
+        update_post_meta($post_id, '_shortcode_value', $shortcode);
+    }
+
+	public function easycal_add_shortcode_column($columns) {
+		$columns['shortcode'] = __('Shortcode', 'easycal');
+		return $columns;
 	}
- 
-	public function easycal_init_post_type() {
-
-		$labels = array(
-			'name'               => 'Shortcodes',
-			'singular_name'      => 'Shortcode',
-			'menu_name'          => 'Shortcodes',
-			'all_items'          => 'Todos los Shortcodes',
-			'add_new'            => 'Agregar Nuevo',
-			'add_new_item'       => 'Agregar Nuevo Shortcode',
-			'edit_item'          => 'Editar Shortcode',
-			'new_item'           => 'Nuevo Shortcode',
-			'view_item'          => 'Ver Shortcode',
-			'search_items'       => 'Buscar Shortcodes',
-			'not_found'          => 'No se encontraron shortcodes',
-			'not_found_in_trash' => 'No se encontraron shortcodes en la papelera',
-		);
 	
-		$args = array(
-			'labels'             => $labels,
-			'public'             => true,
-			'publicly_queryable' => true,
-			'show_ui'            => true,
-			'show_in_menu'       => 'easycal-admin-menu', // Reemplaza 'tu_menu_padre_slug' con el slug de tu menú padre
-			'query_var'          => true,
-			'rewrite'            => array('slug' => 'shortcode'),
-			'capability_type'    => 'post',
-			'has_archive'        => true,
-			'hierarchical'       => false,
-			'menu_position'      => null,
-			'supports'           => array('title', 'editor', 'author', 'excerpt'),
-		);
-	
-		register_post_type('shortcode', $args);
-	}
-		
-	public function easycal_add_admin_menu() {
+    public function easycal_display_shortcode_column($column, $post_id) {
+        if ($column === 'shortcode') {
+            $shortcode = get_post_meta($post_id, '_shortcode_value', true);
+            echo '<input type="text" readonly="readonly" value="' . esc_attr($shortcode) . '" onclick="this.select();" />';
+        }
+    }
 
-		add_menu_page( 
-			__('EasyCal', 'easy-cal'), 
-			__('EasyCal', 'easy-cal'), 
-			'manage_options', 
-			'easycal-admin-menu', 
-			array( $this, 'easycal_admin_menu_page' ), 
-			'dashicons-calendar-alt', 
-			30
-		);
-	}
+    public function easycal_enqueue_styles() {
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/easycal-admin.css', array(), $this->version, 'all');
+    }
 
-	public function easycal_admin_menu_page () {
-
-		$render = '<div class="page-main"></div>';
-
-		echo wp_kses( $render, 'h1' );
-	}
-
-	public function easycal_enqueue_styles() {
-
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/easycal-admin.css', array(), $this->version, 'all' );
-
-	}
-
-	public function easycal_enqueue_scripts() {
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/easycal-admin.js', array( 'jquery' ), $this->version, false );
-
-	}
+    public function easycal_enqueue_scripts() {
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/easycal-admin.js', array('jquery'), $this->version, false);
+    }
 
 }
--- inc/easycal.php
+++ inc/easycal.php
@@ -2,113 +2,106 @@
 
 class Easycal {
 
+    protected $loader;
+    protected $plugin_name;
+    protected $version;
 
-	protected $loader;
+    public function __construct() {
+        $this->init();
+    }
 
-	protected $plugin_name;
+    private function init() {
+        $this->version = defined( 'EASYCAL_VERSION' ) ? EASYCAL_VERSION : '1.0.0';
+        $this->plugin_name = 'easycal';
 
-	protected $version;
+        $this->easycal_load_dependencies();
+        $this->easycal_set_locale();
+        $this->easycal_define_admin_hooks();
+        $this->easycal_define_public_hooks();
+    }
 
-	public function __construct() {
-		$this->init();
-	}
+    private function easycal_load_dependencies() {
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'inc/easycal-loader.php';
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'inc/easycal-i18n.php';
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/easycal-admin.php';
+        require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/easycal-public.php';
 
-	private function init() {
+        if (is_admin()) {
+            $this->loader = new Easycal_Loader();
+        }
+    }
 
-		$this->version = defined( 'EASYCAL_VERSION' ) ? 'EASYCAL_VERSION' : '1.0.0'; 
-		$this->plugin_name = 'easycal';
+    private function easycal_set_locale() {
+        if (is_admin()) {
+            $plugin_i18n = new Easycal_i18n();
 
-		$this->easycal_load_dependencies();
-		$this->easycal_set_locale();
-		$this->easycal_define_admin_hooks();
-		$this->easycal_define_public_hooks();
-	}
+            if (method_exists($plugin_i18n, 'easycal_load_plugin_textdomain')){
+                $this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'easycal_load_plugin_textdomain' );
+            }
+        }
+    }
 
-	private function easycal_load_dependencies() {
-
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'inc/easycal-loader.php';
-
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'inc/easycal-i18n.php';
-
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/easycal-admin.php';
-
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/easycal-public.php';
-
-		if (is_admin()) {
-			$this->loader = new Easycal_Loader();
-		}
-
-	}
-
-	private function easycal_set_locale() {
-
-		if (is_admin()) {
-			$plugin_i18n = new Easycal_i18n();
-
-			if (method_exists($plugin_i18n, 'easycal_load_plugin_textdomain')){
-				$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'easycal_load_plugin_textdomain' );
-			}
-		}
-	}
-
-	private function easycal_define_admin_hooks() {
-
-		if (is_admin()) {
-			$plugin_admin = new Easycal_Admin($this->get_plugin_name(), $this->get_version());
-			$this->easycal_add_admin_hooks($plugin_admin);
-		}
-	}
+    private function easycal_define_admin_hooks() {
+        if (is_admin()) {
+            $plugin_admin = new Easycal_Admin($this->get_plugin_name(), $this->get_version());
+            $this->easycal_add_admin_hooks($plugin_admin);
+        }
+    }
 
 	private function easycal_add_admin_hooks($plugin_admin) {
-		if (method_exists($plugin_admin, 'easycal_enqueue_styles')) {
-			$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'easycal_enqueue_styles');
-		}
-
-		if (method_exists($plugin_admin, 'easycal_enqueue_scripts')) {
-			$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'easycal_enqueue_scripts');
-		}
-
-		if (method_exists($plugin_admin, 'easycal_add_admin_menu')) {
-			$this->loader->add_action('admin_menu', $plugin_admin, 'easycal_add_admin_menu');
-		}
-
-		if (method_exists($plugin_admin, 'easycal_init_post_type')) {
-			$this->loader->add_action('init', $plugin_admin, 'easycal_init_post_type');
-		}
-	}
-
-	private function easycal_define_public_hooks() {
-
-		if(is_admin()){
-			$plugin_public = new Easycal_Public( $this->get_plugin_name(), $this->get_version() );
-			$this->easycal_add_public_hooks($plugin_public);
-		}
-	}
-
-	private function easycal_add_public_hooks($plugin_public){
-		if (method_exists($plugin_public, 'easycal_enqueue_styles')) {
-			$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'easycal_enqueue_styles' );
-		}
+		$styles = method_exists($plugin_admin, 'easycal_enqueue_styles') ? 
+			$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'easycal_enqueue_styles') : null;
 		
-		if (method_exists($plugin_public, 'easycal_enqueue_scripts')){
-			$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'easycal_enqueue_scripts' );
-		}
+		$scripts = method_exists($plugin_admin, 'easycal_enqueue_scripts') ? 
+			$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'easycal_enqueue_styles') : null;
+		
+		$admin_menu = method_exists($plugin_admin, 'easycal_add_admin_menu') ? 
+			$this->loader->add_action('admin_menu', $plugin_admin, 'easycal_add_admin_menu') : null;
+		
+		$post_type = method_exists($plugin_admin, 'easycal_init_post_type') ? 
+			$this->loader->add_action('init', $plugin_admin, 'easycal_init_post_type') : null;
+		
+		$generic = method_exists($plugin_admin, 'easycal_generate_and_save_shortcode') ? 
+			$this->loader->add_action('save_post', $plugin_admin, 'easycal_generate_and_save_shortcode') : null;
+		
+		$add = method_exists($plugin_admin, 'easycal_add_shortcode_column') ? 
+			$this->loader->add_action('save_post', $plugin_admin, 'easycal_add_shortcode_column') : null;
+		
+		$display = method_exists($plugin_admin, 'easycal_display_shortcode_column') ? 
+			$this->loader->add_action('manage_shortcode_posts_custom_column', $plugin_admin, 'easycal_display_shortcode_column', 10, 2) : null;
 	}
+	
+    private function easycal_define_public_hooks() {
+        if(is_admin()){
+            $plugin_public = new Easycal_Public( $this->get_plugin_name(), $this->get_version() );
+            $this->easycal_add_public_hooks($plugin_public);
+        }
+    }
 
-	public function easycal_run() {
-		$this->loader->easycal_run();
-	}
+    private function easycal_add_public_hooks($plugin_public){
+        if (method_exists($plugin_public, 'easycal_enqueue_styles')) {
+            $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'easycal_enqueue_styles' );
+        }
 
-	public function get_plugin_name() {
-		return $this->plugin_name;
-	}
+        if (method_exists($plugin_public, 'easycal_enqueue_scripts')){
+            $this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'easycal_enqueue_scripts' );
+        }
+    }
 
-	public function get_loader() {
-		return $this->loader;
-	}
+    public function easycal_run() {
+        $this->loader->easycal_run();
+    }
 
-	public function get_version() {
-		return $this->version;
-	}
+    public function get_plugin_name() {
+        return $this->plugin_name;
+    }
+
+    public function get_loader() {
+        return $this->loader;
+    }
+
+    public function get_version() {
+        return $this->version;
+    }
 
 }
